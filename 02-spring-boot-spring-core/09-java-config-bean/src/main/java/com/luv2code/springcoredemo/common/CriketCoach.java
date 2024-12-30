9881f8ff4289c5997a680b60818741ae4fd4d991
Dang Nguyen <nguyen05082004@gmail.com>
2024-12-31 01:31:27 +0700

    add spring boot spring core

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CriketCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CriketCoach implements Coach{
+
+    public CriketCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes!!!-)";
+    }
+}
